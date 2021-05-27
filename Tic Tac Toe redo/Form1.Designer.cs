63823c876aef89b499a6cdd8a6d5041843fb66b1
Liane Delos Reyes <ishtroberiii@gmail.com>
2021-05-27 12:16:51 +0800

    Edited

--- Tic Tac Toe redo/Form1.Designer.cs
+++ Tic Tac Toe redo/Form1.Designer.cs
@@ -49,126 +49,144 @@ namespace Tic_Tac_Toe_redo
             // 
             // A1
             // 
+            this.A1.BackColor = System.Drawing.Color.MediumPurple;
             this.A1.Font = new System.Drawing.Font("MS Reference Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.A1.Location = new System.Drawing.Point(12, 58);
             this.A1.Name = "A1";
             this.A1.Size = new System.Drawing.Size(78, 79);
             this.A1.TabIndex = 0;
-            this.A1.UseVisualStyleBackColor = true;
+            this.A1.UseVisualStyleBackColor = false;
             this.A1.Click += new System.EventHandler(this.button_Click);
             // 
             // B1
             // 
+            this.B1.BackColor = System.Drawing.Color.MediumPurple;
             this.B1.Font = new System.Drawing.Font("MS Reference Sans Serif", 36F, System.Drawing.FontStyle.Bold);
             this.B1.Location = new System.Drawing.Point(12, 143);
             this.B1.Name = "B1";
             this.B1.Size = new System.Drawing.Size(78, 79);
             this.B1.TabIndex = 1;
-            this.B1.UseVisualStyleBackColor = true;
+            this.B1.UseVisualStyleBackColor = false;
             this.B1.Click += new System.EventHandler(this.button_Click);
             // 
             // C1
             // 
+            this.C1.BackColor = System.Drawing.Color.MediumPurple;
             this.C1.Font = new System.Drawing.Font("MS Reference Sans Serif", 36F, System.Drawing.FontStyle.Bold);
             this.C1.Location = new System.Drawing.Point(12, 228);
             this.C1.Name = "C1";
             this.C1.Size = new System.Drawing.Size(78, 79);
             this.C1.TabIndex = 2;
-            this.C1.UseVisualStyleBackColor = true;
+            this.C1.UseVisualStyleBackColor = false;
             this.C1.Click += new System.EventHandler(this.button_Click);
             // 
             // C2
             // 
+            this.C2.BackColor = System.Drawing.Color.MediumPurple;
             this.C2.Font = new System.Drawing.Font("MS Reference Sans Serif", 36F, System.Drawing.FontStyle.Bold);
             this.C2.Location = new System.Drawing.Point(96, 228);
             this.C2.Name = "C2";
             this.C2.Size = new System.Drawing.Size(78, 79);
             this.C2.TabIndex = 5;
-            this.C2.UseVisualStyleBackColor = true;
+            this.C2.UseVisualStyleBackColor = false;
             this.C2.Click += new System.EventHandler(this.button_Click);
             // 
             // B2
             // 
+            this.B2.BackColor = System.Drawing.Color.MediumPurple;
             this.B2.Font = new System.Drawing.Font("MS Reference Sans Serif", 36F, System.Drawing.FontStyle.Bold);
             this.B2.Location = new System.Drawing.Point(96, 143);
             this.B2.Name = "B2";
             this.B2.Size = new System.Drawing.Size(78, 79);
             this.B2.TabIndex = 4;
-            this.B2.UseVisualStyleBackColor = true;
+            this.B2.UseVisualStyleBackColor = false;
             this.B2.Click += new System.EventHandler(this.button_Click);
             // 
             // A2
             // 
+            this.A2.BackColor = System.Drawing.Color.MediumPurple;
             this.A2.Font = new System.Drawing.Font("MS Reference Sans Serif", 36F, System.Drawing.FontStyle.Bold);
             this.A2.Location = new System.Drawing.Point(96, 58);
             this.A2.Name = "A2";
             this.A2.Size = new System.Drawing.Size(78, 79);
             this.A2.TabIndex = 3;
-            this.A2.UseVisualStyleBackColor = true;
+            this.A2.UseVisualStyleBackColor = false;
             this.A2.Click += new System.EventHandler(this.button_Click);
             // 
             // C3
             // 
+            this.C3.BackColor = System.Drawing.Color.MediumPurple;
             this.C3.Font = new System.Drawing.Font("MS Reference Sans Serif", 36F, System.Drawing.FontStyle.Bold);
             this.C3.Location = new System.Drawing.Point(180, 228);
             this.C3.Name = "C3";
             this.C3.Size = new System.Drawing.Size(78, 79);
             this.C3.TabIndex = 8;
-            this.C3.UseVisualStyleBackColor = true;
+            this.C3.UseVisualStyleBackColor = false;
             this.C3.Click += new System.EventHandler(this.button_Click);
             // 
             // B3
             // 
+            this.B3.BackColor = System.Drawing.Color.MediumPurple;
             this.B3.Font = new System.Drawing.Font("MS Reference Sans Serif", 36F, System.Drawing.FontStyle.Bold);
             this.B3.Location = new System.Drawing.Point(180, 143);
             this.B3.Name = "B3";
             this.B3.Size = new System.Drawing.Size(78, 79);
             this.B3.TabIndex = 7;
-            this.B3.UseVisualStyleBackColor = true;
+            this.B3.UseVisualStyleBackColor = false;
             this.B3.Click += new System.EventHandler(this.button_Click);
             // 
             // A3
             // 
+            this.A3.BackColor = System.Drawing.Color.MediumPurple;
             this.A3.Font = new System.Drawing.Font("MS Reference Sans Serif", 36F, System.Drawing.FontStyle.Bold);
             this.A3.Location = new System.Drawing.Point(180, 58);
             this.A3.Name = "A3";
             this.A3.Size = new System.Drawing.Size(78, 79);
             this.A3.TabIndex = 6;
-            this.A3.UseVisualStyleBackColor = true;
+            this.A3.UseVisualStyleBackColor = false;
             this.A3.Click += new System.EventHandler(this.button_Click);
             // 
             // ngameBtn
             // 
+            this.ngameBtn.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.ngameBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ngameBtn.Location = new System.Drawing.Point(12, 12);
             this.ngameBtn.Name = "ngameBtn";
             this.ngameBtn.Size = new System.Drawing.Size(78, 40);
             this.ngameBtn.TabIndex = 9;
             this.ngameBtn.Text = "New Game";
-            this.ngameBtn.UseVisualStyleBackColor = true;
+            this.ngameBtn.UseVisualStyleBackColor = false;
             this.ngameBtn.Click += new System.EventHandler(this.ngameBtn_Click);
             // 
             // resetBtn
             // 
+            this.resetBtn.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.resetBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.resetBtn.Location = new System.Drawing.Point(96, 12);
             this.resetBtn.Name = "resetBtn";
             this.resetBtn.Size = new System.Drawing.Size(78, 40);
             this.resetBtn.TabIndex = 10;
             this.resetBtn.Text = "Reset";
-            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.UseVisualStyleBackColor = false;
             this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
             // 
             // exitBtn
             // 
+            this.exitBtn.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.exitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.exitBtn.Location = new System.Drawing.Point(273, 12);
             this.exitBtn.Name = "exitBtn";
             this.exitBtn.Size = new System.Drawing.Size(78, 40);
             this.exitBtn.TabIndex = 11;
             this.exitBtn.Text = "Exit";
-            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.UseVisualStyleBackColor = false;
             this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
             // 
             // XWin
             // 
+            this.XWin.BackColor = System.Drawing.Color.RosyBrown;
+            this.XWin.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.XWin.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.XWin.Location = new System.Drawing.Point(9, 316);
             this.XWin.Name = "XWin";
             this.XWin.Size = new System.Drawing.Size(100, 23);
@@ -177,6 +195,9 @@ namespace Tic_Tac_Toe_redo
             // 
             // OWin
             // 
+            this.OWin.BackColor = System.Drawing.Color.RosyBrown;
+            this.OWin.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.OWin.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
             this.OWin.Location = new System.Drawing.Point(115, 316);
             this.OWin.Name = "OWin";
             this.OWin.Size = new System.Drawing.Size(100, 23);
@@ -185,6 +206,9 @@ namespace Tic_Tac_Toe_redo
             // 
             // Drawn
             // 
+            this.Drawn.BackColor = System.Drawing.Color.RosyBrown;
+            this.Drawn.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Drawn.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
             this.Drawn.Location = new System.Drawing.Point(251, 316);
             this.Drawn.Name = "Drawn";
             this.Drawn.Size = new System.Drawing.Size(100, 23);
